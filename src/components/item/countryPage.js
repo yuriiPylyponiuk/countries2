e9094eaff5cc0e443496bed4c42476c2c202a11d
Yurii Pylyponiuk <rutzi112@gmail.com>
2020-11-23 16:42:04 +0200

    add Country component, fix some styles

---
+++ src/components/item/countryPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CountryPage extends React.Component{
+  // constructor(props){
+  //   super(props)
+  // }
+  render(){
+    return(
+      <h1>Hello</h1>
+    )
+  }
+}
+
+export default CountryPage;
